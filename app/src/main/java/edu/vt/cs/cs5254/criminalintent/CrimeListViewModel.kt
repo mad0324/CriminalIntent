7caa902aaebd53b24a5017b860d183f35803383c
mad0324 <michaeldavies93@gmail.com>
2022-02-26 22:32:37 -0500

    BNR Chapter 11

--- app/src/main/java/edu/vt/cs/cs5254/criminalintent/CrimeListViewModel.kt
+++ app/src/main/java/edu/vt/cs/cs5254/criminalintent/CrimeListViewModel.kt
@@ -3,13 +3,8 @@ package edu.vt.cs.cs5254.criminalintent
 import androidx.lifecycle.ViewModel
 
 class CrimeListViewModel : ViewModel() {
-    val crimes = mutableListOf<Crime>()
-    init {
-        for (i in 0 until 100) {
-            val crime = Crime()
-            crime.title = "Crime #$i"
-            crime.isSolved = i % 2 == 0
-            crimes += crime
-        }
-    }
+
+    private val crimeRepository = CrimeRepository.get()
+    val crimeListLiveData = crimeRepository.getCrimes()
+
 }
